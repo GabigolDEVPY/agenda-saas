78a38d7439cceb4b4eecb2d83fc76ce92edd0fde
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2026-04-04 13:11:25 -0300

    adicionando arquivos

--- Employee/models.py
+++ Employee/models.py
@@ -4,9 +4,9 @@ from Establishment.models import Establishment
 # Create your models here.
 class Employee(models.Model):
     establishment = models.ForeignKey(Establishment, on_delete=models.CASCADE, related_name='employees')
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=35)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20, blank=True)
+    phone = models.CharField(max_length=11, blank=True)
 
     def __str__(self):
         return self.name
