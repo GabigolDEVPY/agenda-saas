940b536322515147cb1a11a61646b3e0de7faee7
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2026-04-17 18:16:42 -0300

    This reverts commit 730536ecc5acd728a897278d90fd8a976dfd3749.

--- Establishment/urls.py
+++ Establishment/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import PublicAgenda, SaveInfosView, SaveAddressView, SaveOperatingHoursView
+from . views import PublicAgenda, SaveInfosView, SaveAddressView
 
-app_name = "establishment"
+app_name = 'establishment'
 
 urlpatterns = [
-    path("e/<str:uid>/", PublicAgenda.as_view(), name="public_agenda"),
-    path("infos/save/", SaveInfosView.as_view(), name="save_infos"),
-    path("address/save/", SaveAddressView.as_view(), name="save_address"),
-    path("operating-hours/save/", SaveOperatingHoursView.as_view(), name="save_operating_hours"),
+    path('e/<str:uid>/', PublicAgenda.as_view(), name='public_agenda'),
+    path('infos/save/', SaveInfosView.as_view(), name='save_infos'),
+    path('address/save/', SaveAddressView.as_view(), name='save_address'),
 ]
+
