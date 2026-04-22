b13aec408ae81dac9a7a4cb04f060c33bb898c06
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2026-04-21 22:26:02 -0300

    arrumando banco de dados supabase

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+from dotenv import load_dotenv
+import os   
+load_dotenv()  # Carrega as variáveis de ambiente do arquivo .env
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,8 +59,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('SB_NAME'),
+        'USER': os.getenv('SB_USER'),
+        'PASSWORD': os.getenv('SB_PASSWORD'),
+        'HOST': os.getenv('SB_HOST'),
+        'PORT': os.getenv('SB_PORT'),
+        'OPTIONS': {'sslmode': 'require'}
     }
 }
 
